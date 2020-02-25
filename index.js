ceb086e8b1ea59ebecc2f5a6311dfe6984665250
Daniel Droppa <danwdroppa@gmail.com>
2020-02-24 19:32:05 -0500

    updated file structure

---
+++ index.js
@@ -0,0 +1,18 @@
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
+.prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
