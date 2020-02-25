bd48e6a0d79dfd11dcf2eb7766573114c85672ac
Daniel Droppa <danwdroppa@gmail.com>
2020-02-24 19:48:28 -0500

    added prompts

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const questions = [
-
-];
-
-function writeToFile(fileName, data) {
-}
 
 function init() {
-
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "filename",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            message: "Give a brief description of your project.",
+            name: "description",
+        },
+        {
+            type: "checkbox",
+            message: "Do you need a table of contents?",
+            name: "Contents",
+            choices: [
+                "Yes",
+                "No",
+            ]
+        },
+    ]).then(function (data) {
+        var filename = data.filename.trim(" ") + ".MD";
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success!");
+        });
+    });
 }
-
 init();
-.prompt([
-    {
-      type: "input",
-      message: "What is your user name?",
-      name: "username"
-    },
